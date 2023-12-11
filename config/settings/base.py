afaac69ae0988acad0f0b7d837f8b895493d6991
David Vaz <dvaz@evolutio.pt>
2023-12-11 11:27:57 +0000

    Correct settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -137,7 +137,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.common.BrokenLinkEmailsMiddleware",
-    "allauth.account.middleware.AccountMiddleware",
+    # "allauth.account.middleware.AccountMiddleware",
     #"django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
